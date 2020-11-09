fa19fee472ae33959976ac18b7a5aa1a4b9849fd
Tata Satya Pratheek <68555775+TataSatyaPratheek@users.noreply.github.com>
2020-11-09 20:12:59 +0530

    FSA language checker
    FSA language checker for even zeroed binary strings

---
+++ 1fsa.py
@@ -0,0 +1,40 @@
+"""
+General schema 
+to determine if a string is in the language:
+
+way to do it 
+start scanning from the left end
+go over the string one symbol at a time
+reach the end to be ready for the answer
+"""
+
+"""
+plan 
+
+define a class and initialize it with nothing having three attributes:
+startState, currentState and prevState. 
+the class will have isIn function that takes in a string to see if it is 
+in the language, set of all even zeroed binary strings. 
+"""
+
+class Even_lang_checker():
+
+	def __init__(self):
+		self.startState = True
+		self.currentState = True
+		self.prevState = True
+
+	def isIn(self, string):
+		if set(list(string)) != {1,0}:
+			return False
+		else:
+			for letter in string:
+				if letter != 0:
+					self.currentState = self.prevState
+				else:
+					self.currentState = not self.prevState
+			if self.currentState:
+				return True
+			else:
+				return False
+
